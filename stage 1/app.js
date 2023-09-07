37199d8acb7a521cc54ec528bbe1757b3b993a34
fredrickray <fredrickraymond2004@gmail.com>
2023-09-07 11:50:26 +0100

    Done

--- stage 1/app.js
+++ stage 1/app.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const port = process.env.PORT || 7070
-
+const dataRoute = require("./route/dataRoute")
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost/${port}`)
+app.use("/api", dataRoute)
+app.listen(7070, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 })
--- stage 1/controller/dataController.js
+++ stage 1/controller/dataController.js
@@ -1,27 +1,33 @@
 const getData = (req, res) => {
-    const { slack_name, track } = req.query;
-  
-    
+    const { slack_name, track } = req.query;  
     const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
     const utcTime = new Date().toISOString();
+    const githubFileUrl = "https://github.com/fredrickray/Zuri/blob/main/stage%201/app.js";
+    const githubRepoUrl = "https://github.com/fredrickray/Zuri";
+  
+   try {
+        if(!slack_name || !track) {
+            throw ({Error: "Invalid query key"})
+        }
+        else{
+            res.status(200).json(
+                {
+                    slack_name,
+                    current_day: currentDay,
+                    utc_time: utcTime,
+                    track,
+                    github_file_url: githubFileUrl,
+                    github_repo_url: githubRepoUrl,
+                    status_code: 200,
+                  }
+            );
+        }
+   } 
+   catch (error) {
+        res.status(500).send(error)
+   }
   
     
-    const githubUsername = 'fredrickray';
-    const githubRepo = 'https://github.com/fredrickray/Zuri.git';
-    const githubFileUrl = `https://github.com/${githubUsername}/${githubRepo}/Zuri/main/${githubFileName}`;
-    const githubRepoUrl = `https://github.com/${githubUsername}/${githubRepo}`;
-  
-    const responseData = {
-      slack_name,
-      current_day: currentDay,
-      utc_time: utcTime,
-      track,
-      github_file_url: githubFileUrl,
-      github_repo_url: githubRepoUrl,
-      status_code: 200,
-    };
-  
-    res.status(200).json(responseData);
 };
 
 module.exports = {
